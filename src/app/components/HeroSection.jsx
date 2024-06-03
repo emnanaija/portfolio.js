bfbc179667909c70d0da85945bf63c791d4dad2a
emna-naija-pro <emnanaija@gmail.com>
2024-06-03 21:58:47 +0100

    changes

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -36,12 +36,7 @@ const HeroSection = () => {
           </h1>
          
           <div>
-            <Link
-              href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
-            >
-              Hire Me
-            </Link>
+           
             <Link
               href="/cv.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
